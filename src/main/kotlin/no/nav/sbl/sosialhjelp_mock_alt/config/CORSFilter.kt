42077147c5e078d2cede430d91a795e463cb529d
siljee <silje.hansen3@nav.no>
2020-05-06 11:51:18 +0200

    rename

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/config/CORSFilter.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/config/CORSFilter.kt
@@ -18,7 +18,7 @@ class CORSFilter : Filter {
         val origin = if (servletRequest is HttpServletRequest) (servletRequest.getHeader("Origin")) else null
 
         httpResponse.setHeader("Access-Control-Allow-Origin", origin)
-        httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-XSRF-TOKEN, Authorization, NAV-Call-Id")
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-XSRF-TOKEN, Authorization, Nav-Call-Id")
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true")
         filterChain.doFilter(servletRequest, httpResponse)
