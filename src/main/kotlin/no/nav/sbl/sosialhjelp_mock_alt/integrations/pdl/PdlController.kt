38fb02d1fbec6817a3206f2d97391093dd63cded
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-06-03 12:41:38 +0200

    Legger til default-response for PDLController (for modia-api og innsyn-api)

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/pdl/PdlController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/pdl/PdlController.kt
@@ -1,10 +1,22 @@
 package no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl
 
-import no.nav.sbl.sosialhjelp_mock_alt.integrations.norg.model.NavEnhet
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.Kjoenn
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlFoedselsdato
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlInnsynHentPerson
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlInnsynPerson
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlInnsynPersonResponse
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlKjoenn
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlModiaHentPerson
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlModiaPerson
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlModiaPersonResponse
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlPersonNavn
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlTelefonnummer
 import no.nav.sbl.sosialhjelp_mock_alt.objectMapper
 import no.nav.sbl.sosialhjelp_mock_alt.utils.logger
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.util.MultiValueMap
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,8 +26,44 @@ class PdlController {
     }
 
     @RequestMapping("/pdl_endpoint_url")
-    fun dummyEndpoint(): String {
+    fun dummyEndpoint(@RequestParam parameters: MultiValueMap<String, String>, @RequestBody body: String): String {
         log.info("Henter pdl_endpoint_url")
-        return "OK"
+
+        return decideResponse(body)
     }
+
+    private fun decideResponse(body: String): String {
+        return when {
+            body.contains(Regex("(navn)|(kjoenn)|(telefonnummer)|(foedsel)")) -> {
+                objectMapper.writeValueAsString(defaultResponseModia())
+            }
+            body.contains(Regex("(adressebeskyttelse)")) -> {
+                objectMapper.writeValueAsString(defaultResponseInnsyn())
+            }
+            else -> "OK"
+        }
+    }
+
+    private fun defaultResponseModia() =
+            PdlModiaPersonResponse(
+                    errors = emptyList(),
+                    data = PdlModiaHentPerson(
+                            hentPerson = PdlModiaPerson(
+                                    navn = listOf(PdlPersonNavn("Natalie", null, "Emberland")),
+                                    kjoenn = listOf(PdlKjoenn(Kjoenn.KVINNE)),
+                                    foedsel = listOf(PdlFoedselsdato("1945-10-26")),
+                                    telefonnummer = listOf(PdlTelefonnummer("+47","11112222", 1))
+                            )
+                    )
+            )
+
+    private fun defaultResponseInnsyn() =
+            PdlInnsynPersonResponse(
+                    errors = emptyList(),
+                    data = PdlInnsynHentPerson(
+                            hentPerson = PdlInnsynPerson(
+                                    adressebeskyttelse = emptyList()
+                            )
+                    )
+            )
 }
---
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/pdl/model/PdlCommon.kt
@@ -0,0 +1,51 @@
+package no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model
+
+data class PdlError(
+        val message: String,
+        val locations: List<PdlErrorLocation>,
+        val path: List<String>?,
+        val extensions: PdlErrorExtension
+)
+
+data class PdlErrorLocation(
+        val line: Int?,
+        val column: Int?
+)
+
+data class PdlErrorExtension(
+        val code: String?,
+        val classification: String
+)
+
+data class Adressebeskyttelse(
+        val gradering: Gradering
+)
+
+enum class Gradering {
+    STRENGT_FORTROLIG_UTLAND, // kode 6 (utland)
+    STRENGT_FORTROLIG, // kode 6
+    FORTROLIG, // kode 7
+    UGRADERT
+}
+
+data class PdlPersonNavn(
+        val fornavn: String,
+        val mellomnavn: String?,
+        val etternavn: String
+)
+
+data class PdlKjoenn(
+        val kjoenn: Kjoenn
+)
+
+enum class Kjoenn { MANN, KVINNE, UKJENT }
+
+data class PdlFoedselsdato(
+        val foedselsdato: String?
+)
+
+data class PdlTelefonnummer(
+        val landskode: String,
+        val nummer: String,
+        val prioritet: Int
+)
