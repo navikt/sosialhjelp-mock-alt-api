a6fc17fadcb008d515dbf11058a9e9c4421d1bc5
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2020-11-13 10:18:17 +0100

    Prøver å bytte issuer domain.

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/login/LoginCoockieAndTokenController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/login/LoginCoockieAndTokenController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.net.URL
+import java.util.concurrent.TimeUnit
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -24,7 +25,9 @@ class LoginCoockieAndTokenController(
     fun getMockAltMetadate(): String {
         val fileResourceRetriever = FileResourceRetriever("/metadata.json", "/jwkset.json")
         val retrieveResource = fileResourceRetriever.retrieveResource(URL("http://metadata"))
-        val metadata = retrieveResource.content.replace("http://jwks", "${host_address}sosialhjelp/mock-alt-api/local/jwks")
+        val metadata = retrieveResource.content
+                .replace("http://jwks", "${host_address}sosialhjelp/mock-alt-api/local/jwks")
+                .replace("iss-localhost", "iss-$cookie_domain")
         StsController.log.info("Henter metadata:\n$metadata")
         return metadata
     }
@@ -32,13 +35,20 @@ class LoginCoockieAndTokenController(
     @Unprotected
     @GetMapping("/login/cookie")
     fun addCookie(@RequestParam(value = "subject") subject: String?,
-                  @RequestParam(value = "cookiename", defaultValue = "localhost-idtoken") cookieName: String?,
+                  @RequestParam(value = "cookiename") cookieName: String?,
                   @RequestParam(value = "redirect", required = false) redirect: String?,
                   @RequestParam(value = "expiry", required = false) expiry: String?,
                   request: HttpServletRequest?, response: HttpServletResponse): Cookie? {
-        val expiryTime = expiry?.toLong() ?: JwtTokenGenerator.EXPIRY
-        val token = JwtTokenGenerator.createSignedJWT(subject ?: fastFnr, expiryTime)
-        val cookie = Cookie(cookieName, token.serialize())
+        val expiryTime = expiry?.toLong() ?: 240 // Default = 4 timer
+        val claimSet = JwtTokenGenerator.buildClaimSet(
+                subject ?: fastFnr,
+                "iss-$cookie_domain",
+                "aud-$cookie_domain",
+                JwtTokenGenerator.ACR,
+                TimeUnit.MINUTES.toMillis(expiryTime)
+        )
+        val token = JwtTokenGenerator.createSignedJWT(claimSet)
+        val cookie = Cookie(cookieName ?: "$cookie_domain-idtoken", token.serialize())
         cookie.domain = cookie_domain
         cookie.path = "/"
         response.addCookie(cookie)
