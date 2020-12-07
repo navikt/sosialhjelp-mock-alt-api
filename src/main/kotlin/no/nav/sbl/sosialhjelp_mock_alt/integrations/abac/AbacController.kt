c11428ea0919e5c6a221e7704e051f4736309cd6
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2020-12-07 11:08:26 +0100

    Bytter til post mapping.

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/abac/AbacController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/abac/AbacController.kt
@@ -6,7 +6,7 @@ import no.nav.sbl.sosialhjelp_mock_alt.integrations.abac.model.XacmlResponse
 import no.nav.sbl.sosialhjelp_mock_alt.objectMapper
 import no.nav.sbl.sosialhjelp_mock_alt.utils.logger
 import org.springframework.util.MultiValueMap
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,7 +16,7 @@ class AbacController {
         private val log by logger()
     }
 
-    @GetMapping("/abac/application/authorize")
+    @PostMapping("/abac/application/authorize")
     fun getAuthorization(@RequestParam parameters:MultiValueMap<String, String>): String {
         val authorization = XacmlResponse(response = listOf(AbacResponse(decision = Decision.Permit, associatedAdvice = emptyList())))
         log.info("Henter abac authorization: $authorization")
