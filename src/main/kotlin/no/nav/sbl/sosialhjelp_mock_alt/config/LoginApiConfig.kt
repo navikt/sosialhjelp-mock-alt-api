90858829d656d4b9e5131c6c12bc0ae1a1bdf200
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2021-02-03 10:44:20 +0100

    Støtte til å fake login-api.

---
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/config/LoginApiConfig.kt
@@ -0,0 +1,18 @@
+package no.nav.sbl.sosialhjelp_mock_alt.config
+
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class LoginApiConfig : WebMvcConfigurer {
+
+    @Bean
+    fun restTemplate(): RestTemplate {
+        return RestTemplateBuilder()
+//                .errorHandler(RestTemplateErrorHandler())
+                .build()!!
+    }
+}
--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/otherEndpoints/logg_ukjente_requester/LoggUkjenteRequesterController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/otherEndpoints/logg_ukjente_requester/LoggUkjenteRequesterController.kt
@@ -2,12 +2,14 @@ package no.nav.sbl.sosialhjelp_mock_alt.otherEndpoints.logg_ukjente_requester
 
 import no.nav.sbl.sosialhjelp_mock_alt.objectMapper
 import no.nav.sbl.sosialhjelp_mock_alt.utils.logger
+import org.springframework.context.annotation.Profile
 import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Profile("no-swagger")
 class LoggUkjenteRequesterController {
     companion object {
         private val log by logger()
