6599b89266ce70aa565ed7cd58286007eed30a01
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2021-03-02 16:24:19 +0100

    Fikser VedtakDto. (Stoler mer på frontend.)

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/datastore/bostotte/model/BostotteDto.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/datastore/bostotte/model/BostotteDto.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package no.nav.sbl.sosialhjelp_mock_alt.datastore.bostotte.model
 
 import no.nav.sbl.soknadsosialhjelp.soknad.okonomi.opplysning.JsonOkonomiOpplysningUtbetaling
@@ -28,31 +30,11 @@ data class SakerDto(
 enum class BostotteStatus { UNDER_BEHANDLING, VEDTATT }
 enum class BostotteRolle { HOVEDPERSON, BIPERSON }
 
-data class VedtakDto(val vedtak: Vedtakskode) {
-    val kode: String
-    val beskrivelse: String
-    val type: String
-
-    init {
-        kode = vedtak.name
-        beskrivelse = vedtak.beskrivelse
-        type = vedtak.type
-    }
-
-}
-
-enum class Vedtakskode(val beskrivelse: String, val type: String) {
-    V00("Søknaden din er innvilget.", "INNVILGET"),
-    V02("Du har fått avslag på søknaden din om bostøtte fordi du eller andre i husstanden ikke har rett til bostøtte.", "AVSLAG"),
-    V03("Du har fått avslag på søknaden din om bostøtte fordi du/dere hadde for høy inntekt.", "AVSLAG"),
-    V04("Du har fått avslag på søknaden din om bostøtte fordi boligen din ikke oppfyller kravene.", "AVSLAG"),
-    V05("Du har fått avslag på søknaden din om bostøtte fordi du ikke var registrert på søknadsadressen i folkeregisteret.", "AVSLAG"),
-    V07("Klagen din ble avvist da den ble sendt inn etter klagefristen.", "AVVIST"),
-    V09(" Søknaden din om bostøtte er avvist fordi det mangler opplysninger eller dokumentasjon, eller fordi noen i husstanden er registrert på en annen søknad.", "AVVIST"),
-    V11("Hovedperson er død", "AVSLAG"),
-    V12("For høy anslått inntekt (ikke i bruk lengere)", "AVSLAG"),
-    V48("Søknaden din om bostøtte er avvist fordi noen du bor sammen med ikke er registrert på adressen i folkeregisteret.", "AVSLAG"),
-}
+data class VedtakDto(
+    val kode: String,
+    val beskrivelse: String,
+    val type: String,
+)
 
 data class UtbetalingerDto(
         val belop: Double = randomInt(5).toDouble(),
