a5c8149f26a4d737d92e11c8998c5b69553aba1a
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2021-04-06 09:58:06 +0200

    Nytt woldena endepunkt for å hente ned innsynsfila. (Pluss lagt inn et manglende ende punkt for filopplasting.)

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/innsyn_api/InnsynController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/innsyn_api/InnsynController.kt
@@ -2,15 +2,20 @@ package no.nav.sbl.sosialhjelp_mock_alt.integrations.innsyn_api
 
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.fiks.SoknadService
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.fiks.model.DigisosApiWrapper
+import no.nav.sbl.sosialhjelp_mock_alt.datastore.fiks.model.VedleggMetadata
 import no.nav.sbl.sosialhjelp_mock_alt.objectMapper
 import no.nav.sbl.sosialhjelp_mock_alt.utils.hentFnrFraHeaders
 import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 import java.util.UUID
 
 @RestController
@@ -33,6 +38,23 @@ class InnsynController(private val soknadService: SoknadService) {
                 fnr = faktiskFnr, fiksDigisosIdInput = id, digisosApiWrapper = digisosApiWrapper)
         return ResponseEntity.ok("{\"fiksDigisosId\":\"$id\"}")
     }
+
+    @PostMapping("/innsyn-api/api/v1/digisosapi/{fiksDigisosId}/filOpplasting",
+            consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun filOpplasting(@PathVariable fiksDigisosId: String, @RequestParam("file") file: MultipartFile): ResponseEntity<String> {
+        val vedleggMetadata = VedleggMetadata(file.originalFilename, file.contentType, file.size)
+        val dokumentlagerId = soknadService.lastOppFil(fiksDigisosId, vedleggMetadata, file = file)
+        return ResponseEntity.ok(dokumentlagerId)
+    }
+
+    @GetMapping("/innsyn-api/api/v1/digisosapi/{digisosId}/innsynsfil")
+    fun hentInnsynsfilWoldena(@PathVariable digisosId: String): ResponseEntity<ByteArray> {
+        val soknad = soknadService.hentSoknad(digisosId) ?: return ResponseEntity.noContent().build()
+        val innsynsfil = soknadService.hentDokument(digisosId, soknad.digisosSoker!!.metadata)
+                ?: return ResponseEntity.noContent().build()
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(innsynsfil.toByteArray())
+    }
+
 
     private fun hentFnrFraTokenOrInput(fnrInput: String?, headers: HttpHeaders): String {
         return fnrInput ?: hentFnrFraHeaders(headers)
