a7524ce2f0595694e44729063510cfe4091ca739
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-05-05 09:15:11 +0200

    støtter PDLs sokAdresse søke-tjeneste (gjøres likt som tps adressesøk)

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/pdl/PdlController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/pdl/PdlController.kt
@@ -1,8 +1,10 @@
 package no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl
 
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.feil.FeilService
+import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.PdlAdresseSokService
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.PdlService
-import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.PdlRequest
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.HentPersonRequest
+import no.nav.sbl.sosialhjelp_mock_alt.integrations.pdl.model.SokAdresseRequest
 import no.nav.sbl.sosialhjelp_mock_alt.objectMapper
 import no.nav.sbl.sosialhjelp_mock_alt.utils.hentFnrFraToken
 import no.nav.sbl.sosialhjelp_mock_alt.utils.logger
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class PdlController(
     private val pdlService: PdlService,
+    private val pdlAdresseSokService: PdlAdresseSokService,
     private val feilService: FeilService,
 ) {
     @PostMapping("/pdl_endpoint_url", produces = ["application/json;charset=UTF-8"])
@@ -25,12 +28,27 @@ class PdlController(
         @RequestBody body: String,
         @RequestHeader headers: HttpHeaders,
     ): String {
-        val tokenFnr = hentFnrFraToken(headers)
-        val pdlRequest = objectMapper.readValue(body, PdlRequest::class.java)
-        if (tokenFnr != pdlRequest.variables.ident) {
-            log.warn("Token matcher ikke request! $tokenFnr != ${pdlRequest.variables.ident}")
+        val ident = hentFnrFraToken(headers)
+        return handleRequest(body, ident)
+    }
+
+    private fun handleRequest(body: String, ident: String): String {
+        if (body.contains("ident") && body.contains("historikk")) {
+            val hentPersonRequest = objectMapper.readValue(body, HentPersonRequest::class.java)
+            return handleHentPersonRequest(hentPersonRequest, ident)
         }
-        return decideResponse(pdlRequest)
+        if (body.contains("paging") && body.contains("criteria")) {
+            val sokAdresseRequest = objectMapper.readValue(body, SokAdresseRequest::class.java)
+            return handleSokAdresseRequest(sokAdresseRequest, ident)
+        }
+        return "OK"
+    }
+
+    private fun handleHentPersonRequest(hentPersonRequest: HentPersonRequest, ident: String): String {
+        if (ident != hentPersonRequest.variables.ident) {
+            log.warn("Token matcher ikke request! $ident != ${hentPersonRequest.variables.ident}")
+        }
+        return decideResponse(hentPersonRequest)
     }
 
     /**
@@ -41,35 +59,41 @@ class PdlController(
      * navn -> del av request fra innsyn-api (gjelder også 3 av de over, men denne inneholder verken forelderBarnRelasjon, folkeregisterpersonstatus eller bostedsadresse)
      * adressebeskyttelse -> tilgangskontroll-sjekk kall fra soknad-api
      */
-    private fun decideResponse(pdlRequest: PdlRequest): String {
-        val fnr = pdlRequest.variables.ident
+    private fun decideResponse(hentPersonRequest: HentPersonRequest): String {
+        val fnr = hentPersonRequest.variables.ident
         return when {
-            pdlRequest.query.contains(Regex("(forelderBarnRelasjon)")) -> {
+            hentPersonRequest.query.contains(Regex("(forelderBarnRelasjon)")) -> {
                 feilService.eventueltLagFeil(fnr, "PdlController", "getSoknadPerson")
                 objectMapper.writeValueAsString(pdlService.getSoknadPersonResponseFor(fnr))
             }
-            pdlRequest.query.contains(Regex("(folkeregisterpersonstatus)")) -> {
+            hentPersonRequest.query.contains(Regex("(folkeregisterpersonstatus)")) -> {
                 feilService.eventueltLagFeil(fnr, "PdlController", "getSoknadBarn")
                 objectMapper.writeValueAsString(pdlService.getSoknadBarnResponseFor(fnr))
             }
-            pdlRequest.query.contains(Regex("(bostedsadresse)")) -> {
+            hentPersonRequest.query.contains(Regex("(bostedsadresse)")) -> {
                 feilService.eventueltLagFeil(fnr, "PdlController", "getSoknadEktefelle")
                 objectMapper.writeValueAsString(pdlService.getSoknadEktefelleResponseFor(fnr))
             }
-            pdlRequest.query.contains(Regex("(kjoenn)")) -> {
+            hentPersonRequest.query.contains(Regex("(kjoenn)")) -> {
                 feilService.eventueltLagFeil(fnr, "PdlController", "getModia")
                 objectMapper.writeValueAsString(pdlService.getModiaResponseFor(fnr))
             }
-            pdlRequest.query.contains(Regex("(navn)")) -> {
+            hentPersonRequest.query.contains(Regex("(navn)")) -> {
                 feilService.eventueltLagFeil(fnr, "PdlController", "getInnsyn")
                 objectMapper.writeValueAsString(pdlService.getInnsynResponseFor(fnr))
             }
-            pdlRequest.query.contains(Regex("(adressebeskyttelse)")) -> {
+            hentPersonRequest.query.contains(Regex("(adressebeskyttelse)")) -> {
                 feilService.eventueltLagFeil(fnr, "PdlController", "getSoknadAdressebeskyttelse")
                 objectMapper.writeValueAsString(pdlService.getSoknadAdressebeskyttelseResponseFor(fnr))
             }
             else -> "OK"
         }
+    }
+
+    private fun handleSokAdresseRequest(sokAdresseRequest: SokAdresseRequest, ident: String): String {
+        val postnummer = sokAdresseRequest.variables.criteria.first { it.fieldName == "postnummer" }.searchRule["equals"] ?: ""
+        feilService.eventueltLagFeil(ident, "PdlController", "getSokAdresse")
+        return objectMapper.writeValueAsString(pdlAdresseSokService.getAdresse(postnummer))
     }
 
     companion object {
