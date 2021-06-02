07327c16a05ef94016270b1d865721a1dea2a33a
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-06-02 08:46:18 +0200

    config for token-validation-spring-test. - tar i bruk mock-oauth2-server - LoginCookieAndTokenController tilbyr nye endepunkter utad med støtte for flere issuers - FssToken genereres vha MockOAuth2Server

---
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/config/TokenConfig.kt
@@ -0,0 +1,17 @@
+package no.nav.sbl.sosialhjelp_mock_alt.config
+
+import no.nav.security.token.support.spring.test.MockOAuth2ServerAutoConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+
+@Configuration
+@Import(MockOAuth2ServerAutoConfiguration::class)
+class TokenConfig {
+
+    // Mulig starte mockOAuth2Server manuelt fremfor med AutoConfiguration.
+    //
+    // LoginCookieAndTokenController
+    // - wellknown for issuer -> /login/metadata/{issuer}  -> gå til discoveryurl for issuer i mockOauth2Server
+    // - jwks for issuer      -> /login/jwks/{issuer}      -> gå til jwks url for issuer i mockOauth2Server
+    // - addCookie            -> /login/cookie/{issuer}    -> kopiert og tilpasset fra MockLoginController
+}
--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/appgw/AppGatewaySTSController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/appgw/AppGatewaySTSController.kt
@@ -4,6 +4,7 @@ import no.nav.sbl.sosialhjelp_mock_alt.integrations.aareg.model.FssToken
 import no.nav.sbl.sosialhjelp_mock_alt.objectMapper
 import no.nav.sbl.sosialhjelp_mock_alt.utils.hentFnrFraHeaders
 import no.nav.sbl.sosialhjelp_mock_alt.utils.logger
+import no.nav.security.mock.oauth2.MockOAuth2Server
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class AppGatewaySTSController {
+class AppGatewaySTSController(
+    private val mockOAuth2Server: MockOAuth2Server
+) {
     companion object {
         private val log by logger()
     }
@@ -19,7 +22,7 @@ class AppGatewaySTSController {
     @GetMapping("/appgw/")
     fun getAaregSts(@RequestHeader headers: HttpHeaders): ResponseEntity<FssToken> {
         val fnr = hentFnrFraHeaders(headers)
-        val fssToken = FssToken.createToken(fnr)
+        val fssToken = FssToken(mockOAuth2Server.issueToken("selvbetjening", fnr, "someaudience").serialize(), "FssToken", 120L)
         log.info("Henter appgw token: ${objectMapper.writeValueAsString(fssToken)}")
         return ResponseEntity.ok(fssToken)
     }
