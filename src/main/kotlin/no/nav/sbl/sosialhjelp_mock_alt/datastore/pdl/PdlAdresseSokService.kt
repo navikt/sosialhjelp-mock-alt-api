10545ff9e15b540d3eb635c0ae121c53433745df
martintveter <41987225+martintveter@users.noreply.github.com>
2021-08-25 11:51:09 +0200

    legger til default testdata for adressesok (30 første resultater for "sanner*" hentet fra dev) (#110)
    Testet OK i dev. Evt forbedringer kommer senere

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/datastore/pdl/PdlAdresseSokService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/datastore/pdl/PdlAdresseSokService.kt
@@ -1,8 +1,6 @@
 package no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl
 
-import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.model.AdresseDto
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.model.AdresseSokHit
-import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.model.ForenkletBostedsadresse
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.model.PdlAdresseSok
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.model.PdlAdresseSokResponse
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.model.PdlAdresseSokResult
@@ -12,44 +10,32 @@ import org.springframework.stereotype.Component
 @Component
 class PdlAdresseSokService {
 
-    private val adresseMap: HashMap<String, PdlAdresseSokResponse> = HashMap()
+    // key: "<vegadresse><husnummer><husbokstav>"
+    private val nyAdresseMap: HashMap<String, AdresseSokHit> = HashMap()
 
-    fun getAdresse(postnummer: String): PdlAdresseSokResponse? {
-        log.info("Henter PDL adresse for postnummer: $postnummer")
-        return adresseMap[postnummer] ?: PdlAdresseSokResponse.defaultResponse()
+    init {
+        val hits = PdlAdresseSokResponse.defaultResponse().data.sokAdresse?.hits
+        hits?.let { hit -> nyAdresseMap.putAll(hit.map { it.vegadresse.adressenavn + it.vegadresse.husnummer + it.vegadresse.husbokstav to it }) }
     }
 
-    fun putAdresse(
-        postnummer: String,
-        adresse: ForenkletBostedsadresse,
-        geografiskTilknytning: String? = "0315"
-    ) {
-        adresseMap[postnummer] = PdlAdresseSokResponse(
-            errors = null,
-            data = PdlAdresseSok(
-                sokAdresse = PdlAdresseSokResult(
-                    hits = listOf(
-                        AdresseSokHit(
-                            score = 0f,
-                            vegadresse = AdresseDto(
-                                matrikkelId = "matrikkelId",
-                                husnummer = adresse.husnummer,
-                                husbokstav = if (adresse.husbokstav.isNullOrBlank()) null else adresse.husbokstav,
-                                adressenavn = adresse.adressenavn,
-                                kommunenavn = "Test kommune",
-                                kommunenummer = adresse.kommunenummer,
-                                postnummer = postnummer,
-                                poststed = "poststed",
-                                bydelsnummer = geografiskTilknytning
-                            )
-                        )
-                    ),
-                    pageNumber = 1,
-                    totalPages = 1,
-                    totalHits = 1
+    fun getAdresse(sokestreng: String): PdlAdresseSokResponse {
+        log.info("PDL adressesok, sokestreng: $sokestreng")
+        val treff = nyAdresseMap.filter { (key, _) -> key.startsWith(sokestreng, ignoreCase = true) }.values
+        return if (treff.isNotEmpty()) {
+            PdlAdresseSokResponse(
+                errors = null,
+                data = PdlAdresseSok(
+                    sokAdresse = PdlAdresseSokResult(
+                        hits = treff.toList(),
+                        pageNumber = 1,
+                        totalHits = treff.size,
+                        totalPages = 1
+                    )
                 )
             )
-        )
+        } else {
+            PdlAdresseSokResponse.defaultResponse()
+        }
     }
 
     companion object {
