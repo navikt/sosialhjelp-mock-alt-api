f4d8bbe3ab05f3465fbdc57f6a19139f3dcb8f51
Line Moseng <line.moseng@nav.no>
2021-09-07 11:58:45 +0200

    bump jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,7 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "1.8"
+            jvmTarget = "11"
         }
     }
 
