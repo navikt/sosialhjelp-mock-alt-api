f724e9f250d02beff969f7ac4596c97cc34b9f39
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2021-11-04 11:59:28 +0100

    DialogStatus for deg som ikke vil kjøre opp flere apper enn nødvendig.

---
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/otherEndpoints/dialogApi/DialogStatus.kt
@@ -0,0 +1,4 @@
+package no.nav.sbl.sosialhjelp_mock_alt.otherEndpoints.dialogApi
+
+@Suppress("unused")
+class DialogStatus(val ident: String, val tilgangTilDialog: Boolean, val antallUlesteMeldinger: Int)
---
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/otherEndpoints/dialogApi/DialogStatusRequest.kt
@@ -0,0 +1,3 @@
+package no.nav.sbl.sosialhjelp_mock_alt.otherEndpoints.dialogApi
+
+class DialogStatusRequest(val ident: String)
