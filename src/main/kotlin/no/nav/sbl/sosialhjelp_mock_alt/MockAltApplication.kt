d5b1a5f679dfe93d3efe4d76528f73c801ce0147
linemos <line.moseng@nav.no>
2022-02-25 09:56:48 +0100

    Bump filformat og fix deprecated constructor (#151)
    * bump filformat og fix deprecated constructor
    
    * ktlint

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/MockAltApplication.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/MockAltApplication.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpObjectMapper
 import no.nav.sbl.sosialhjelp_mock_alt.config.CORSFilter
@@ -21,7 +22,17 @@ class MockAltApplication {
 }
 
 val objectMapper: ObjectMapper = JsonSosialhjelpObjectMapper.createObjectMapper()
-    .registerModules(JavaTimeModule(), KotlinModule())
+    .registerModules(
+        JavaTimeModule(),
+        KotlinModule.Builder()
+            .withReflectionCacheSize(512)
+            .configure(KotlinFeature.NullToEmptyCollection, false)
+            .configure(KotlinFeature.NullToEmptyMap, false)
+            .configure(KotlinFeature.NullIsSameAsDefault, false)
+            .configure(KotlinFeature.SingletonSupport, false)
+            .configure(KotlinFeature.StrictNullChecks, false)
+            .build()
+    )
     .configure(SerializationFeature.INDENT_OUTPUT, true)
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
