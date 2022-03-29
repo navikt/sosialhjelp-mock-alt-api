bdf2a2e165159a3131b61c39cc3c46b1ab2fbd9d
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2022-03-29 13:35:59 +0200

    Legger til støtte for abacløs Modia.

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/azure/AzureController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/azure/AzureController.kt
@@ -8,6 +8,7 @@ import no.nav.sbl.sosialhjelp_mock_alt.integrations.azure.model.AzureAdGrupper
 import no.nav.sbl.sosialhjelp_mock_alt.utils.MockAltException
 import no.nav.sbl.sosialhjelp_mock_alt.utils.hentFnrFraCookieNoDefault
 import no.nav.sbl.sosialhjelp_mock_alt.utils.hentFnrFraHeadersNoDefault
+import no.nav.sbl.sosialhjelp_mock_alt.utils.hentFnrFraToken
 import no.nav.sbl.sosialhjelp_mock_alt.utils.hentFnrFraTokenNoDefault
 import no.nav.sbl.sosialhjelp_mock_alt.utils.logger
 import org.springframework.http.HttpHeaders
@@ -48,43 +49,61 @@ class AzureController(val pdlService: PdlService) {
         }
     }
 
-    @GetMapping("/azuread/graph/users/{id}/memberOf")
-    fun getAzureBrukersGrupper(@PathVariable id: String): ResponseEntity<AzureAdGrupper> {
-        log.info("Henter azureAd brukers grupper for brukerId $id")
+    @GetMapping("/azuread/graph/me/memberOf")
+    fun getInloggetAzureBrukersGrupper(@RequestHeader headers: HttpHeaders): ResponseEntity<AzureAdGrupper> {
+        val id = hentFnrFraToken(headers)
+        log.info("Henter azureAd brukers grupper for \"me\" (id: $id)")
         return try {
-            val personalia = pdlService.getPersonalia(id)
-            val grupper = if (personalia.navn.fornavn == "Admin") {
-                if (personalia.navn.etternavn == "Admin") {
-                    listOf(
-                        AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-veileder", "Veiledere"),
-                        AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-admin", "Administratorer"),
-                    )
-                } else if (personalia.navn.etternavn == "Arkiv") {
-                    listOf(
-                        AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-arkiv", "Teknisk-arkiv"),
-                    )
-                } else if (personalia.navn.etternavn == "Innsikt") {
-                    listOf(
-                        AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-innsikt", "Innsikktere"),
-                    )
-                } else {
-                    listOf(
-                        AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-veileder", "Veiledere"),
-                        AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-admin", "Administratorer"),
-                        AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-arkiv", "Teknisk-arkiv"),
-                        AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-innsikt", "Innsikktere"),
-                    )
-                }
-            } else if (personalia.navn.fornavn == "Tyske") {
-                emptyList() // Ikke veileder
-            } else {
-                listOf(AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-veileder", "Veiledere"))
-            }
+            val grupper = hentGrupper(id)
             ResponseEntity.ok(AzureAdGrupper(value = grupper))
         } catch (e: MockAltException) {
             log.info("Feil ved henting av brukers grupper: ${e.message}")
             ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
         }
+    }
+
+    @GetMapping("/azuread/graph/users/{id}/memberOf")
+    fun getAzureBrukersGrupper(@PathVariable id: String): ResponseEntity<AzureAdGrupper> {
+        log.info("Henter azureAd brukers grupper for brukerId $id")
+        return try {
+            val grupper = hentGrupper(id)
+            ResponseEntity.ok(AzureAdGrupper(value = grupper))
+        } catch (e: MockAltException) {
+            log.info("Feil ved henting av brukers grupper: ${e.message}")
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+        }
+    }
+
+    private fun hentGrupper(id: String): List<AzureAdGruppe> {
+        val personalia = pdlService.getPersonalia(id)
+        val grupper = if (personalia.navn.fornavn == "Admin") {
+            if (personalia.navn.etternavn == "Admin") {
+                listOf(
+                    AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-veileder", "Veiledere"),
+                    AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-admin", "Administratorer"),
+                )
+            } else if (personalia.navn.etternavn == "Arkiv") {
+                listOf(
+                    AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-arkiv", "Teknisk-arkiv"),
+                )
+            } else if (personalia.navn.etternavn == "Innsikt") {
+                listOf(
+                    AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-innsikt", "Innsikktere"),
+                )
+            } else {
+                listOf(
+                    AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-veileder", "Veiledere"),
+                    AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-admin", "Administratorer"),
+                    AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-arkiv", "Teknisk-arkiv"),
+                    AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-innsikt", "Innsikktere"),
+                )
+            }
+        } else if (personalia.navn.fornavn == "Tyske") {
+            emptyList() // Ikke veileder
+        } else {
+            listOf(AzureAdGruppe("0000-MOCK-sosialhjelp-dialog-veileder", "Veiledere"))
+        }
+        return grupper
     }
 
     @GetMapping("/azuread/graph/groups/{id}/members")
