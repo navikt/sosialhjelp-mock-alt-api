0c16a6bdbd9e15240ed96c1f7e038a4c50556d9e
martintveter <41987225+martintveter@users.noreply.github.com>
2022-04-26 09:45:46 +0200

    fjern sts-kode (#171)

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/aareg/model/FssToken.kt
+++
@@ -1,3 +0,0 @@
-package no.nav.sbl.sosialhjelp_mock_alt.integrations.aareg.model
-
-data class FssToken(val access_token: String, val token_type: String, val expires_in: Long)
--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/integrations/sts/model/STSResponse.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.sbl.sosialhjelp_mock_alt.integrations.sts.model
-
-data class STSResponse(
-    val access_token: String,
-    val token_type: String,
-    val expires_in: Int
-)
