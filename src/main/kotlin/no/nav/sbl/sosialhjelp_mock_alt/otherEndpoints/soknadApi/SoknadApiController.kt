968e7b6827ae83f017231a7bf7ce4811f1e2a73e
martintveter <41987225+martintveter@users.noreply.github.com>
2022-09-27 11:53:46 +0200

    legger til mocking av svarut-integrasjon (#201)
    * legger til mocking av svarut-integrasjon.
    
    /soknadoversikt/soknader endepunkt returnerer nå info om svarut-søknader, slik at man også kan se disse ved lokal kjøring.
    
    * sett SOKNAD_FRONTEND_BASEURL som env variabel, hvis man ønsker korrekt lenke til ettersendelse ved lokal kjøring
    
    * Lenker til felles-dok i readme

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/otherEndpoints/soknadApi/SoknadApiController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/otherEndpoints/soknadApi/SoknadApiController.kt
@@ -1,9 +1,11 @@
 package no.nav.sbl.sosialhjelp_mock_alt.otherEndpoints.soknadApi
 
+import no.nav.sbl.sosialhjelp_mock_alt.datastore.fiks.SvarUtService
 import no.nav.sbl.sosialhjelp_mock_alt.datastore.pdl.PdlService
 import no.nav.sbl.sosialhjelp_mock_alt.utils.MockAltException
 import no.nav.sbl.sosialhjelp_mock_alt.utils.hentFnrFraToken
 import no.nav.sbl.sosialhjelp_mock_alt.utils.logger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -12,10 +14,15 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 @RestController
 class SoknadApiController(
     private val pdlService: PdlService,
+    private val svarUtService: SvarUtService,
+    @Value("\${soknad-frontend-baseurl}") private val soknadFrontendBaseUrl: String
 ) {
     companion object {
         private val log by logger()
@@ -40,10 +47,24 @@ class SoknadApiController(
         }
     }
 
+    /**
+     * Endepunktet kalles kun av innsyn-api ved lokal kjøring
+     */
     @GetMapping("soknad-api/soknadoversikt/soknader")
     @ResponseBody
     fun soknadoversikt(@RequestHeader headers: HttpHeaders): ResponseEntity<List<SaksListeDto>> {
-        // dummy endepunkt som returnerer 200 OK med en tom liste.
-        return ResponseEntity.ok(emptyList())
+        val ident = hentFnrFraToken(headers)
+        val svarUtSoknader = svarUtService.getSvarUtSoknader(ident)
+            .map {
+                val soknadId = it.first.eksternReferanse.removePrefix("-")
+                SaksListeDto(
+                    fiksDigisosId = null,
+                    soknadTittel = "Økonomisk sosialhjelp ($soknadId)",
+                    sistOppdatert = Date.from(LocalDateTime.parse(it.second.innsendingstidspunkt, DateTimeFormatter.ISO_DATE_TIME).toInstant(ZoneOffset.UTC)),
+                    kilde = "soknad-api",
+                    url = "$soknadFrontendBaseUrl/skjema/$soknadId/ettersendelse"
+                )
+            }
+        return ResponseEntity.ok(svarUtSoknader)
     }
 }
