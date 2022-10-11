f4f4c54a51588e8eb1750f0d4c384c0d87750fd1
Ida <25682301+idaons@users.noreply.github.com>
2022-10-11 09:13:09 +0200

    Fjerner dialog ting (#203)
    * refaktorer rolle-struktur fra frontend
    * fjerner dialog-roller
    * fjern sist innsendte soknad

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/otherEndpoints/dialogApi/DialogStatus.kt
+++
@@ -1,4 +0,0 @@
-package no.nav.sbl.sosialhjelp_mock_alt.otherEndpoints.dialogApi
-
-@Suppress("unused")
-class DialogStatus(val ident: String, val tilgangTilDialog: Boolean, val antallUlesteMeldinger: Int)
