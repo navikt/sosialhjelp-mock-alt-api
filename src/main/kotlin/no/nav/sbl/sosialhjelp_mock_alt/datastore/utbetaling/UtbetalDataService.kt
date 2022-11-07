a4dfbe39cf2fc63f74b48170fede0fcfcceeea2f
hildenok <hilde.nokland@nav.no>
2022-11-07 13:23:40 +0100

    Opprydding - utkommentert kode.

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/datastore/utbetaling/UtbetalDataService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/datastore/utbetaling/UtbetalDataService.kt
@@ -27,25 +27,6 @@ class UtbetalDataService {
         autoGenerationSet.add(fnr)
     }
 
-//    private val utbetalingListMap: HashMap<String, List<UtbetalingDto>> = HashMap()
-//    private val autoGenerationSet: HashSet<String> = HashSet()
-//
-//    fun getUtbetalingerFraNav(ident: String): List<UtbetalingDto> {
-//        log.info("Henter utbetalinger for $ident")
-//        if (autoGenerationSet.contains(ident)) {
-//            return listOf(UtbetalingDto(netto = 12000.0, utbetalingsdato = LocalDate.now().minusDays(14)))
-//        }
-//        return utbetalingListMap[ident] ?: listOf(UtbetalingDto())
-//    }
-//
-//    fun putUtbetalingerFraNav(ident: String, utbetalinger: List<UtbetalingDto>) {
-//        utbetalingListMap[ident] = utbetalinger
-//    }
-//
-//    fun enableAutoGenerationFor(fnr: String) {
-//        autoGenerationSet.add(fnr)
-//    }
-
     companion object {
         private val log by logger()
     }
