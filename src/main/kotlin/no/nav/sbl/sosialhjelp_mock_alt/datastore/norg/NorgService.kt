e0d6d38879c9dc7ba40725daf023dc4c4d354678
Ida Onshus <25682301+idaons@users.noreply.github.com>
2023-06-26 07:36:13 +0200

    format2

--- src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/datastore/norg/NorgService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp_mock_alt/datastore/norg/NorgService.kt
@@ -6,37 +6,40 @@ import org.springframework.stereotype.Service
 @Service
 class NorgService {
 
-    private val navEnheter = mutableMapOf<String, NavEnhet>()
-    private val gtNavEnheter = mutableMapOf<String, NavEnhet>()
+  private val navEnheter = mutableMapOf<String, NavEnhet>()
+  private val gtNavEnheter = mutableMapOf<String, NavEnhet>()
 
-    init {
-        val sentrum = lagMockNavEnhet("0301", "Sentrum")
-        val grunerokka = lagMockNavEnhet("0315", "Grünerløkka")
-        val aarstad = lagMockNavEnhet("1208", "NAV Årstad")
-        val bergenhus = lagMockNavEnhet("1209", "NAV Bergenhus")
-        val ytrebygda = lagMockNavEnhet("1210", "NAV Ytrebygda")
-        val horten = lagMockNavEnhet("0701", "NAV Horten")
+  init {
+    val sentrum = lagMockNavEnhet("0301", "Sentrum")
+    val grunerokka = lagMockNavEnhet("0315", "Grünerløkka")
+    val aarstad = lagMockNavEnhet("1208", "NAV Årstad")
+    val bergenhus = lagMockNavEnhet("1209", "NAV Bergenhus")
+    val ytrebygda = lagMockNavEnhet("1210", "NAV Ytrebygda")
+    val horten = lagMockNavEnhet("0701", "NAV Horten")
 
-        navEnheter[sentrum.enhetNr] = sentrum
-        navEnheter[grunerokka.enhetNr] = grunerokka
-        navEnheter[aarstad.enhetNr] = aarstad
-        navEnheter[bergenhus.enhetNr] = bergenhus
-        navEnheter[ytrebygda.enhetNr] = ytrebygda
+    navEnheter[sentrum.enhetNr] = sentrum
+    navEnheter[grunerokka.enhetNr] = grunerokka
+    navEnheter[aarstad.enhetNr] = aarstad
+    navEnheter[bergenhus.enhetNr] = bergenhus
+    navEnheter[ytrebygda.enhetNr] = ytrebygda
 
-        gtNavEnheter["0301"] = sentrum
-        gtNavEnheter["4601"] = bergenhus
-        gtNavEnheter["3801"] = horten
-    }
+    gtNavEnheter["0301"] = sentrum
+    gtNavEnheter["4601"] = bergenhus
+    gtNavEnheter["3801"] = horten
+  }
 
-    fun getNavenhet(enhetsnr: String): NavEnhet? {
-        return navEnheter[enhetsnr] ?: lagMockNavEnhet(enhetsnr, "Generert mockenhet $enhetsnr, Mock kommune")
-    }
+  fun getNavenhet(enhetsnr: String): NavEnhet? {
+    return navEnheter[enhetsnr]
+        ?: lagMockNavEnhet(enhetsnr, "Generert mockenhet $enhetsnr, Mock kommune")
+  }
 
-    fun getNavEnhetForGt(geografiskTilknytning: String): NavEnhet {
-        return gtNavEnheter[geografiskTilknytning] ?: lagMockNavEnhet(geografiskTilknytning, "Generert mockenhet for gt $geografiskTilknytning, Mock kommune")
-    }
+  fun getNavEnhetForGt(geografiskTilknytning: String): NavEnhet {
+    return gtNavEnheter[geografiskTilknytning]
+        ?: lagMockNavEnhet(
+            geografiskTilknytning, "Generert mockenhet for gt $geografiskTilknytning, Mock kommune")
+  }
 
-    fun getAlleNavenheter(): Collection<NavEnhet> {
-        return navEnheter.values
-    }
+  fun getAlleNavenheter(): Collection<NavEnhet> {
+    return navEnheter.values
+  }
 }
