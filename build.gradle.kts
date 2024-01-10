8a6a4e0fb7896d32e6f4da7dcd52c28f962fcf1a
Martin Almvik <martin.almvik@nav.no>
2024-01-10 13:07:20 +0100

    Fjern ubrukte versjonsnummer i build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,6 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val springBootVersion = "3.1.5"
-val coroutinesVersion = "1.7.3"
-val sosialhjelpCommonVersion = "1.20231004.1011-d57fe70"
-val filformatVersion = "1.2023.09.05-13.49-b12f0a7b2b4a"
-val tokenValidationVersion = "3.1.7"
-val jacksonVersion = "2.15.3"
-val springdocversion = "2.2.0"
-val jsonSmartVersion = "2.5.0"
-val mockOauth2ServerVersion = "2.0.1"
-val junitVersion = "4.13.2"
-val log4jVersion = "2.19.0"
-val snakeyamlVersion = "2.0"
-val svarUtVersion = "2.0.0"
-val bouncyCastle = "1.74"
-val netty = "4.1.94.Final"
-
 plugins {
   `jvm-test-suite`
   alias(libs.plugins.kotlin.jvm)
