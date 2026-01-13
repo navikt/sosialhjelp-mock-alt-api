52c79f4e7c5ed79838adef2d0d3b82348e7f8399
Amund Sørumshagen <amund.sorumshagen@nav.no>
2026-01-13 12:20:13 +0100

    Import

--- src/main/kotlin/no/nav/sbl/sosialhjelp/mock/alt/integrations/wellknown/WellKnownController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelp/mock/alt/integrations/wellknown/WellKnownController.kt
@@ -12,10 +12,10 @@ import no.nav.security.mock.oauth2.MockOAuth2Server
 import no.nav.security.token.support.core.configuration.ProxyAwareResourceRetriever
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
